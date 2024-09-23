55a3017a38fb9ee5797abab9a81b5fbf686ff7e9
Tony Andy Oehme <tony-andy.oehme@tum.de>
2024-09-24 00:16:46 +0700

    hahah

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
+import { inter } from '@/app/ui/fonts'
+import '@/app/ui/global.css'
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${inter.className} antialiased`}>{children}</body>
+        </html>
+    );
 }
